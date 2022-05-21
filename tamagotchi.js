7ec3b57d068a421ea4def4c6e0999b47e14586fa
Katie McGowan <katiemcgowan@Matts-MacBook-Air.local>
2022-05-21 10:58:21 -0700

    Adds animation effects to sprites, adds countdowns and clickable buttons for boredom, hunger, and sleepiness

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,3 +1,7 @@
+let boredomMeter = 0;
+let hungerMeter = 0;
+let sleepinessMeter = 0;
+
 const tamagotchi = {
   name: "", 
   age: 0,
@@ -10,15 +14,53 @@ const hatchTamagotchi = (value) => {
   tamagotchi.name = value;
 };
 
+const startBoredomTimer = () => {
+  let counter = setInterval(function() {
+    boredomMeter++;
+    console.log(boredomMeter);
+    $('#boredom').html(boredomMeter);
+    if (boredomMeter >= 10) {
+      clearInterval(counter)
+      console.log(tamagotchi.name + " has died of boredom");
+    }
+  },1000)
+}
+
+const startHungerTimer = () => {
+  let counter = setInterval(function() {
+    hungerMeter++;
+    $('#hunger').html(hungerMeter);
+    if (hungerMeter >= 10) {
+      clearInterval(counter)
+      console.log(tamagotchi.name + " has died of hunger");
+    }
+  },2000)
+}
+
+const startSleepinessTimer = () => {
+  let counter = setInterval(function() {
+    sleepinessMeter++;
+    $('#sleepiness').html(sleepinessMeter);
+    if (sleepinessMeter >= 10) {
+      clearInterval(counter)
+      console.log(tamagotchi.name + " has died of sleepiness");
+    }
+  },3000)
+}
+
 $(".submitname").on("click", function(){
   let tamagotchiName = $("#name").val()
   hatchTamagotchi(tamagotchiName);
-    $(".topscreen-start").addClass("none")
-    $("topscreen-gameplay").removeClass("none");
+    $("#topscreen-start").addClass("none")
+    $("#topscreen-gameplay").removeClass("none");
     $(".egg").addClass("none");
     $(".creature").removeClass("none");
-    $(".bottomscreen-start").addClass("none");
-    $(".bottomscreen-gameplay").removeClass("none")
+    $("#bottomscreen-start").addClass("none");
+    $("#bottomscreen-gameplay").removeClass("none")
+    $("#playername").html(tamagotchiName);
+   startBoredomTimer(); 
+   startHungerTimer();
+   startSleepinessTimer();
 });
 
 
@@ -34,3 +76,26 @@ $(".submitname").on("click", function(){
 //   }, 1000);
 // }
 
+
+
+$("#play").on("click", function () {
+  if (boredomMeter >= 3) {
+    boredomMeter = boredomMeter - 3; 
+    $('#boredom').html(boredomMeter);
+  } else return;
+})
+
+$("#feed").on("click", function () {
+  if (hungerMeter >= 2){
+    hungerMeter = hungerMeter - 2; 
+    $('#hunger').html(hungerMeter);
+  } else return;
+})
+
+$("#rest").on("click", function () {
+  if (sleepinessMeter >= 4) {
+    sleepinessMeter = sleepinessMeter - 4; 
+    $('#sleepiness').html(sleepinessMeter);
+    $("#screen").addClass(".nightscreen");
+  } else return;  
+})
